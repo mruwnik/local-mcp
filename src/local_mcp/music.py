64cdb90cb77bbe13d80280f9c41ccdcaf2251c50
Daniel O'Connell <github@ahiru.pl>
2025-10-28 15:00:31 +0100

    random songs command

--- src/local_mcp/music.py
+++ src/local_mcp/music.py
@@ -1,10 +1,16 @@
-from typing import TypedDict
-from local_mcp.base import mcp
-from local_mcp.settings import ROMPR_API_USER, ROMPR_API_PASSWORD
-import httpx
 import base64
-from bs4 import BeautifulSoup
+import random
+import re
+import time
+from typing import TypedDict
+from urllib.parse import unquote
 
+import httpx
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+from local_mcp.base import mcp
+from local_mcp.settings import CACHE_TIMEOUT, ROMPR_API_PASSWORD, ROMPR_API_USER
 
 BASE_URL = "https://media.ahiru.pl/music/api"
 SONG_ATTRIBUTES = [
@@ -85,6 +91,13 @@ async def mpd_player_command(commands: list[list[str]]) -> dict:
         return response.json()
 
 
+def select_text(element: Tag, selector: str) -> str:
+    val = element.select(selector)
+    if not val:
+        return ""
+    return val[0].text
+
+
 async def get_files(client: httpx.AsyncClient, path: str = "") -> Directory:
     params = {"path": path} if path else {}
     response = await client.get(
@@ -98,19 +111,23 @@ async def get_files(client: httpx.AsyncClient, path: str = "") -> Directory:
     soup = BeautifulSoup(response.text, "html.parser")
     tracks = [
         File(
-            file=str(track.get("name") or ""),
-            title=track.select(".expand")[0].text,
-            duration=track.select(".tracktime")[0].text,
+            file=unquote(str(track.get("name") or "")),
+            title=select_text(track, ".expand"),
+            duration=select_text(track, ".tracktime"),
         )
         for track in soup.select(".clicktrack")
     ]
     folders = [
         Folder(
-            folder=str(
-                folder.select("input", type="hidden", name="dirpath")[0].get("value")
-                or ""
+            folder=unquote(
+                str(
+                    folder.select("input", type="hidden", name="dirpath")[0].get(
+                        "value"
+                    )
+                    or ""
+                )
             ),
-            title=folder.select(".expand")[0].text,
+            title=select_text(folder, ".expand"),
         )
         for folder in soup.select(".clickalbum")
     ]
@@ -165,6 +182,67 @@ async def mpd_play_tracks(
         )
         response.raise_for_status()
         return response.json()
+
+
+cache = {}
+
+
+def should_skip(path: str, skip: str | None) -> bool:
+    return bool(skip and re.search(skip, path)) or "The Dresden Files" in path
+
+
+async def get_all_files(
+    client: httpx.AsyncClient, path: str = "", skip: str | None = None
+) -> list[File]:
+    key = (path, skip)
+    if key in cache:
+        res, ts = cache[key]
+        if time.time() - ts < CACHE_TIMEOUT:
+            return res
+
+    res = []
+    if not should_skip(path, skip):
+        directory = await get_files(client, path)
+        all_files = [file for file in directory["files"]]
+        for directory in directory["directories"]:
+            all_files += await get_all_files(client, directory["folder"], skip)
+        res = [file for file in all_files if not should_skip(file["file"], skip)]
+
+    cache[key] = (res, time.time())
+    return res
+
+
+@mcp.tool()
+async def mdp_play_random_tracks(
+    path: str = "",
+    count: int = 10,
+    clear_first: bool = True,
+    start_playing: bool = True,
+    skip: str | None = None,
+) -> dict:
+    """
+    Play random tracks from a directory.
+
+    This works recursively, by first getting all files from all subdirectories, then choosing `count` random files from the list.
+
+    Args:
+        path: Path to the directory to play tracks from
+        count: Number of tracks to play
+        clear_first: Whether to clear playlist first (default: True)
+        start_playing: Whether to start playing after adding (default: True)
+        skip: a regular expression to skip files
+            If provided, the file will be skipped if it matches the regular expression.
+
+    Returns player status.
+    """
+    async with httpx.AsyncClient() as client:
+        all_files = await get_all_files(client, path, skip)
+        random_files = random.choices(all_files, k=min(count, len(all_files)))
+        return await mpd_play_tracks(
+            sorted([unquote(file["file"]) for file in random_files]),
+            clear_first,
+            start_playing,
+        )
 
 
 @mcp.tool()
--- src/local_mcp/settings.py
+++ src/local_mcp/settings.py
@@ -9,3 +9,5 @@ ROMPR_API_PASSWORD = os.getenv("ROMPR_API_PASSWORD")
 
 USERNAME = os.getenv("LOCAL_MCP_USER")
 PASSWORD = os.getenv("LOCAL_MCP_PASSWORD")
+
+CACHE_TIMEOUT = int(os.getenv("CACHE_TIMEOUT", 60 * 60 * 24 * 7))  # 7 days
